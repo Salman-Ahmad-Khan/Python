6d24e70c290765a7cdf0842ca56acefcb235e0d4
Salman Khan <connect.celman@gmail.com>
2021-10-21 21:17:11 +0530

    updated array.py and renamed python.pdf

--- arrays.py
+++ arrays.py
@@ -1,8 +1,8 @@
-# to use arrays and its corresponding functions we have to first import it
+# to use arrays and its corresponding functions we have first import it
 # import array as arr
 # import array
 from array import *   # '*' will import all the functions from 'array module'
-vals = array('i', [5, 2, 1, -7, 9, 3, 4]) # array.array(typecode,[initializer])
+# vals = array('i', [5, 2, 1, -7, 9, 3, 4]) # array.array(typecode,[initializer])
 # print(vals)
 #
 # vals.reverse()
@@ -10,9 +10,9 @@ vals = array('i', [5, 2, 1, -7, 9, 3, 4]) # array.array(typecode,[initializer])
 #
 # print(vals.buffer_info())   #it will print the address and size of array
 # print(vals.typecode)  # it will print the typecode of array
-
+#
 # print(vals[0])   # it will print first element of array
-
+#
 # this will print array elements one by one
 # for i in vals:
 #     print(i)
@@ -37,7 +37,7 @@ vals = array('i', [5, 2, 1, -7, 9, 3, 4]) # array.array(typecode,[initializer])
 
 # copy an array
 # newary = array(vals.typecode, (a for a in vals))
-newary = array(vals.typecode, (a*a for a in vals))   # the 'newary' will be the sqaure of 'vals' array
+# newary = array(vals.typecode, (a * a for a in vals))   # the 'newary' will be the sqaure of 'vals' array
 # print("copied array: ", newary)
 
 # using for loop
@@ -45,10 +45,36 @@ newary = array(vals.typecode, (a*a for a in vals))   # the 'newary' will be the 
 #     print(i)
 
 # using while loop
-i = 0
-while i <= 6:  # while i < len(newary):
-    print(newary[i])
-    i = i+1
+# i = 0
+# while i <= 6:  # while i < len(newary):
+#     print(newary[i])
+#     i = i+1
+
+
+# take elements from user and search for any number
+arr = array('i', [])
+n = int(input("Enter the size of array "))
+print("Enter the array elements of size:",n)
+for i in range(n):
+    x = int(input())
+    arr.append(x)
+
+print("Array elements are :")
+print(arr)
+
+srch = int(input("Enter the number for search "))
+
+k = 0
+for e in arr:
+    if e == srch:
+        print(k+1, "is the index number of", srch)
+        break
+    k += 1
+
+# search by using a function
+print(arr.index(srch) + 1, 'is the index number of', srch)
+
+
 
 
 
